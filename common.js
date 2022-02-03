5ed50f8c39b25d7e01040a52a2930bc1118a26e4
dondonjang <46506126+dondonjang@users.noreply.github.com>
2022-02-03 18:20:07 +0900

    xss filter code add

---
+++ common.js
@@ -0,0 +1,10 @@
+function XSSCheck(text, type) {
+//type에 따라 제거할지, 치환할지 선택
+if (type == 'remove') {
+text = text.replace(/\<|\>|\"|\'|\%|\;|\(|\)|\&|\+|\-/g,"");
+} else if (type == undefined && type == 'replace') {
+text = text.replace(/\</g, "&lt;");
+text = text.replace(/\>/g, "&gt;");
+}
+return text;
+}
